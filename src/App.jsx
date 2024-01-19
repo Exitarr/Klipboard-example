c768f8ad8470e6371efb1766052de8257ac62242
Exitarr <souravdas212002@gmail.com>
2024-01-19 20:07:40 +0530

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import Header from "./components/Header"
 import BoxSection from "./components/BoxSection"
 import Footer from "./components/Footer"
 import ListContextProvider from "./store/list-context"
+import { useEffect } from "react"
 
 
 function App() {
+  useEffect(() => {
+  alert('This is Just an Example of a Chrome Extension built with ReactJS and chromeAPIs and lacks the features of a full fledged chrome extension like storing urls and redriecting to them.');
+  }, []);
   return (
     <ListContextProvider >
       <Header />
