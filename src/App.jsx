22479d8ffe7a82d7c3f59ef6c1b84b96ca567b66
Exitarr <souravdas212002@gmail.com>
2024-01-19 19:56:29 +0530

    finals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header"
-import BoxSection from "./components/boxSection"
+import BoxSection from "./components/BoxSection"
 import Footer from "./components/Footer"
 import ListContextProvider from "./store/list-context"
 
